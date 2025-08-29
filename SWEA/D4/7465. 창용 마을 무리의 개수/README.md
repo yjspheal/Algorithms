8c5e7a2c85a2109da135f27ba22ccd582124e1df
Youjeong Noh <141787221+yjspheal@users.noreply.github.com>
2025-08-29 16:09:19 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 112 ms, Memory: 61,696 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/README.md
@@ -0,0 +1,15 @@
+# [D4] 창용 마을 무리의 개수 - 7465 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWngfZVa9XwDFAQU) 
+
+### 성능 요약
+
+메모리: 61,696 KB, 시간: 112 ms, 코드길이: 1,726 Bytes
+
+### 제출 일자
+
+2025-08-29 16:09
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -0,0 +1,54 @@
+# 7465. 창용 마을 무리의 개수
+
+# import sys
+# 
+# sys.stdin = open('input.txt')
+
+def dfs(start_p, adj_list, check_group):
+    """
+    시작 사람 번호와 전체 방문 여부(group_list)를 인자로 받아
+    시작 사람이 속하는 무리의 사람들을 모두 group_list True로 만들어 return하는 함수
+
+    Args:
+        start_p : 시작 사람
+        adj_list : 인접리스트
+        check_group : 무리 생성 여부가 담긴 boolean 리스트
+    """
+    stack = [start_p]  # 시작 사람을 스택에 넣고 시작
+
+    while stack:
+        curr_p = stack.pop()      # 사람 하나 꺼내서
+
+        if not check_group[curr_p]:       # 무리가 없는 사람이면
+            check_group[curr_p] = True     # 무리에 넣어준다
+
+            for next_p in adj_list[curr_p]:  # 연결된 사람들도 똑같이 진행한다
+                if not check_group[next_p]:
+                    stack.append(next_p)
+
+    return check_group   # 현재 무리여부 체크 상태를 보여준다
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    # 사람 수, 관계 수
+    people, relationship = map(int, input().split())
+
+    cy_adj = [[] for _ in range(people + 1)]  # 창용관계도 인접리스트
+    for _ in range(relationship):  # 관계 수만큼
+        p1, p2 = map(int, input().split())  # 사람 페어
+
+        cy_adj[p1].append(p2)  # 서로를 추가해준다
+        cy_adj[p2].append(p1)
+
+    cy_group = [False] * (people + 1)     # 사람 체크 여부, 즉 무리가 있는 사람들
+    # 1번부터 people번까지의 사람을 돌며
+    count_group = 0     # 무리 수=0
+    for person in range(1, people + 1):
+        if not cy_group[person]:        # person이 아직 무리가 없다면
+            count_group += 1        # 무리 수 + 1
+            cy_group = dfs(person, cy_adj, cy_group)       # dfs돌려서 무리 체크 상태를 업데이트
+
+
+
+    print(f'#{tc} {count_group}')
